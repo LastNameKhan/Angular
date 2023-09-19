93c185de38fbe2095336f5f6acebd6690bde129e
Aman Khan <Aman Khan@inindcrws.com>
2023-09-19 17:37:15 +0530

    Initial Commmit

---
+++ wishkart/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
