c7d2b40fe09e32b13ae1b3ac241cfb4244b956b3
Aman Khan <Aman Khan@inindcrws.com>
2023-09-26 12:08:14 +0530

    attribute binding

--- wishkart/src/app/product-list/product-list.component.ts
+++ wishkart/src/app/product-list/product-list.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  name="Aman Khan";
+  product = {
+  name : "iPhone 15",
+  price : 789,
+  color : "Red",
+  discount: 8.5,
+  inStock: 10,
+  pImage: '/assets/images/iphone.jpeg'
+  
+}
+
+getDiscountedPrice () {
+  return this.product.price - (this.product.price * this.product.discount / 100)
+}
 
 }
