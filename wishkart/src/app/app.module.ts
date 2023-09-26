c5ce496829843800ba7710f2ba287f4db03cbe16
Aman Khan <Aman Khan@inindcrws.com>
2023-09-26 11:16:03 +0530

    Data Binding

--- wishkart/src/app/app.module.ts
+++ wishkart/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
